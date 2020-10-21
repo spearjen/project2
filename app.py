cbd503408b8ad88454c8c056a44bde6536bc9602
Jay Spears <j.spears.denver@gmail.com>
2020-10-21 08:47:53 -0600

    we're on flask rendered!

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 #import necessary libraries
-from flask import Flask
+from flask import Flask, render_template, redirect
 from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
 from google.cloud import bigquery
-from flask_cors import CORS
 import os
 
 # import otherroute! which is another py file
 
-# try:
-#     # The typical way to import flask-cors
-#     from flask.cor import cross_origin
-# except ImportError:
-#     # Path hack allows examples to be run without installation.
-#     import os
-#     parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#     os.sys.path.insert(0, parentdir)
+try:
+    # The typical way to import flask-cors
+    from flask_cors import CORS
+except ImportError:
+    # Path hack allows examples to be run without installation.
+    import os
+    parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.sys.path.insert(0, parentdir)
 
-#     from flask.cors import cross_origin
+    from flask_cors import CORS
 
 
 
 #create Flask app instance
 app = Flask(__name__)
+api = Api(app)
 cors=CORS(app, resources=r'smallSst.json', allow_headers='Content-Type')
 
 @app.route("/",methods=['GET'])
